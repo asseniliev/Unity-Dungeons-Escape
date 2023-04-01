8968f63e68dd334b36dc55facb6d50321af0d1e7
asseniliev <assen.s.iliev@gmail.com>
2023-04-01 22:33:31 +0200

    - Optimize the jump mechanics

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,7 +8,10 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     //get handle to rigidbody
     private Rigidbody2D rigidBody;
-    private readonly string horizontalAxis = "Horizontal";    
+    private readonly string horizontalAxis = "Horizontal";
+    private float distanceToGroundWhenGrounded;
+    private float distanceToGroundTolerance = 1.05f;
+    private bool isGrounded;
 
     [SerializeField] private float jumpForce = 5f;
     [SerializeField] private float playerSpeed = 2;
@@ -17,6 +20,8 @@ public class Player : MonoBehaviour
     void Start()
     {
         this.rigidBody = GetComponent<Rigidbody2D>();
+        this.distanceToGroundWhenGrounded = getDistanceToGround() * distanceToGroundTolerance;
+        this.isGrounded = true;
     }
 
     // Update is called once per frame
@@ -29,7 +34,7 @@ public class Player : MonoBehaviour
     private void Move()
     {
         float input = Input.GetAxisRaw(horizontalAxis);
-        if(Mathf.Abs(this.rigidBody.velocity.y) < 10E-5)
+        if(isGrounded)
         {
             this.rigidBody.velocity = new Vector2(input * playerSpeed, this.rigidBody.velocity.y);
         }  
@@ -38,9 +43,31 @@ public class Player : MonoBehaviour
     private void Jump()
     {
         //In the check below rigidBody.velocity.y is very small only if the player is not in the air
-        if (Input.GetKeyDown(KeyCode.Space) && Mathf.Abs(this.rigidBody.velocity.y) < 10E-5)        
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            this.rigidBody.AddForce(new Vector2(0f, this.jumpForce), ForceMode2D.Impulse);
+            isGrounded = false;
+            this.rigidBody.AddForce(new Vector2(0f, this.jumpForce), ForceMode2D.Impulse);            
+            StartCoroutine(UpdateJumpStatus());
         }
     }
+
+    private float getDistanceToGround()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(this.transform.position, Vector2.down, 10f, floorLayer);        
+        return hit.distance;        
+    }
+
+    IEnumerator UpdateJumpStatus()
+    {
+        yield return new WaitForSeconds(0.06f);
+
+        while (getDistanceToGround() > distanceToGroundWhenGrounded)
+        {
+            yield return new WaitForSeconds(0.05f);
+        }
+
+        isGrounded = true;
+    }
+
+    
 }
