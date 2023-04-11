418680d0ce8ac8147c4551fcb31345a639f34872
asseniliev <assen.s.iliev@gmail.com>
2023-04-11 22:44:32 +0200

    - Script Moss_Giant movement

--- Assets/Scripts/Enemy/Moss_Giant.cs
+++ Assets/Scripts/Enemy/Moss_Giant.cs
@@ -4,22 +4,49 @@ using UnityEngine;
 
 public class Moss_Giant : Enemy
 {
+    private Transform moveTarget;    
+
+    void Start()
+    {
+        this.moveTarget = this.pointB;
+    }
 
 
     public override void Update()
     {
-        throw new System.NotImplementedException();
+        Move();
     }
 
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     public override void Attack()
     {
-        throw new System.NotImplementedException();
+        //throw new System.NotImplementedException();
+    }
+
+    private void SwapMoveTarget()
+    {
+        if (this.moveTarget.name == this.pointA.name)
+            this.moveTarget = this.pointB;
+        else
+            this.moveTarget = this.pointA;        
+    }
+
+    private void Move()
+    {
+        Debug.Log(this.moveTarget);
+        float step = this.speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(this.transform.position, this.moveTarget.position, step);        
+        if(TargetPositionReached())
+        {
+            SwapMoveTarget();
+        }
+    }
+
+    private bool TargetPositionReached()
+    {
+        Debug.Log(Vector3.Distance(this.transform.position, this.moveTarget.position));
+        return Vector3.Distance(this.transform.position, this.moveTarget.position) < 0.1f;
     }
 
 }
